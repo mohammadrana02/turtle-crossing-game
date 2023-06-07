dca0c1bef01d6a27057480b3d608feeec9e91bd1
Mohammad Rana <mohammadrana02@gmail.com>
2023-06-07 19:44:07 +0100

    test commit

--- scoreboard.py
+++ scoreboard.py
@@ -3,11 +3,6 @@ from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-# SCOREBOARD BEHAVIOUR
-# Create a scoreboard that keeps track of which level
-# the user is on. Every time the turtle player does a successful
-# crossing, the level should increase. When the turtle hits a car,
-# GAME OVER should be displayed in the centre.
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
